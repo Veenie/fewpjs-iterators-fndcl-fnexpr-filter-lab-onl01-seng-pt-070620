95e25a45bc5fa49ab5c8370acc1251a093209cb0
Raveen K Sharma <raveen08@gmail.com>
2021-01-30 21:41:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 // Code your solution here
 
 
+//case insensitive match
+
 function findMatching(array, str){
 return array.filter(driver => driver.toLowerCase() === str.toLowerCase())
 }
 
+
+//match first letter
+
 function fuzzyMatch(array, str){
 return array.filter(driver => driver[0] === str[0])
 }
+
+
+//match if it has object properties
 
 function matchName(array, str){
 return array.filter(driver => driver.name === str)
